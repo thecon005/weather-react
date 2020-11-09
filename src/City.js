c8735ad2ae20a97225c4e90b771ecb230758c84e
thecon005 <71850617+thecon005@users.noreply.github.com>
2020-11-08 21:13:13 -0800

    Added City JS & CSS

---
+++ src/City.js
@@ -0,0 +1,14 @@
+import React from "React";
+import "/src/Stylesheets/City.css";
+
+export default function City(){
+return (
+<h1 id = "city"><strong>
+        Vancouver
+      </strong>
+      </h1>
+      <h2 id = "date">
+      Wednesday October 28, 2020</h2>
+<h3 id = "time">9:00am</h3>
+);
+}
